df567ce7d4522f9a170e42dcd76b5f73fed6555c
andermsilva <67654030+andermsilva@users.noreply.github.com>
2023-09-09 13:48:30 -0300

    version 0.02

---
+++ Semana5/scriptjs.js
@@ -0,0 +1,137 @@
+
+      /*-------------------------------------- Fuções --------------------------------------*/
+      let somaHomens = 0;
+      let somaMulheres = 0;
+      let total = 0;
+
+      function addM() {
+        somaMulheres++;
+        divNum2.setAttribute('value', somaMulheres);
+
+        total = total + 1;
+        divNumT.setAttribute('value', total);
+      }
+      function lessM() {
+
+        if (somaMulheres > 0) {
+
+          somaMulheres--;
+          divNum2.setAttribute('value', somaMulheres);
+          total = total - 1;
+          divNumT.setAttribute('value', total);
+        }
+      }
+
+
+
+      function addH() {
+        somaHomens++;
+        divNum.setAttribute('value', somaHomens);
+        total = total + 1;
+        divNumT.setAttribute('value', total);
+      }
+      function lessH() {
+        if (somaHomens > 0) {
+          somaHomens--;
+          divNum.setAttribute('value', somaHomens);
+          total = total - 1;
+          divNumT.setAttribute('value', total);
+        }
+      }
+
+
+      /*------------------------------------- ######## --------------------------------------*/
+      let h1 = document.createElement('h1');
+      h1.textContent='Total';
+      document.getElementById('content').appendChild(h1).setAttribute('class', 'h1_');
+      //ação de reset
+      let image = document.createElement('img');
+      image.src = 'https://cdn-icons-png.flaticon.com/512/126/126502.png';
+      image.setAttribute('onclick', ' location.reload()');
+      document.getElementById('content').appendChild(image).setAttribute('class', 'reflesh');
+
+
+      let divTotal = document.createElement('div');
+      let divNumT = document.createElement('input');
+      divNumT.setAttribute('readonly', true);
+      divNumT.setAttribute('value', total);
+      divTotal.appendChild(divNumT).setAttribute('class', 'numero')
+      document.getElementById('content').appendChild(divTotal).setAttribute('class', 'total');
+
+      let divPicture = document.createElement('div');
+      let picture = document.createElement('img');
+      picture.src = 'https://cdn-icons-png.flaticon.com/512/610/610120.png';
+      divPicture.appendChild(picture).setAttribute('class', 'img_homem');
+      document.getElementById('content').appendChild(divPicture).setAttribute('class', 'picture');
+
+      divPicture = document.createElement('div');
+      picture = document.createElement('img');
+      picture.src = 'https://cdn-icons-png.flaticon.com/512/180/180675.png';
+      divPicture.appendChild(picture).setAttribute('class', 'img_homem');
+      document.getElementById('content').appendChild(divPicture).setAttribute('class', 'picture');
+
+      let divScore = document.createElement('div');
+
+      //ação adicionar...
+      let divBtn = document.createElement('div');
+      image = document.createElement('img');
+      image.src = 'https://cdn-icons-png.flaticon.com/512/4315/4315609.png';
+      image.setAttribute('onclick', 'addH();');
+
+      divBtn.appendChild(image).setAttribute('class', 'mais');
+      divScore.appendChild(divBtn).setAttribute('class', 'bts');
+
+      // ação subtrair...
+      divBtn = document.createElement('div');
+      image = document.createElement('img');
+      image.src = 'https://cdn-icons-png.flaticon.com/512/5974/5974627.png';
+      image.setAttribute('onclick', 'lessH();');
+
+      divBtn.appendChild(image).setAttribute('class', 'menos');
+      divScore.appendChild(divBtn).setAttribute('class', 'bts');
+
+      let title = document.createElement('div');
+      title.textContent = "Homens";
+      divScore.appendChild(title).setAttribute('class', 'titulo');
+
+      divNum = document.createElement('input');
+      divNum.setAttribute('readonly', true);
+
+
+      divNum.setAttribute('value', somaHomens);
+      divScore.appendChild(divNum).setAttribute('class', 'numero');
+      document.getElementById('content').appendChild(divScore).setAttribute('class', 'score');
+
+      //////====================================================================================////////
+      divScore = document.createElement('div');
+
+      //ação adicionar...
+      divBtn = document.createElement('div');
+      image = document.createElement('img');
+      image.src = 'https://cdn-icons-png.flaticon.com/512/4315/4315609.png';
+
+      image.setAttribute('onclick', 'addM();');
+
+      divBtn.appendChild(image).setAttribute('class', 'mais');
+      divScore.appendChild(divBtn).setAttribute('class', 'bts');
+
+      // ação subtrair...
+      divBtn = document.createElement('div');
+      image = document.createElement('img');
+      image.src = 'https://cdn-icons-png.flaticon.com/512/5974/5974627.png';
+      image.setAttribute('onclick', 'lessM();');
+
+      divBtn.appendChild(image).setAttribute('class', 'menos');
+      divScore.appendChild(divBtn).setAttribute('class', 'bts');
+
+      title = document.createElement('div');
+      title.textContent = "Mulheres";
+      divScore.appendChild(title).setAttribute('class', 'titulo');
+
+      divNum2 = document.createElement('input');
+      divNum2.setAttribute('readonly', true);
+
+      divNum2.setAttribute('value', somaMulheres);
+      divScore.appendChild(divNum2).setAttribute('class', 'numero');
+
+      document.getElementById('content').appendChild(divScore).setAttribute('class', 'score')
